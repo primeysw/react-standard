b8499af0721ab8ce1a0b0056eb86d2fb454e6e07
Seonwoo <primenumber_@naver.com>
2023-02-28 17:16:04 +0900

    craco.config.js file format modified

--- craco.config.js
+++ craco.config.js
@@ -2,7 +2,13 @@
  * @description [Override] : 웹팩 설정
  * @tutorial https://www.npmjs.com/package/@craco/craco
  */
+
 const path = require("path");
+
 module.exports = {
-    webpack: { alias: { "@": path.resolve(__dirname, "./src") } },
+    webpack: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 };
